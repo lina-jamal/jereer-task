64f90d5f18c608fe0d71ea30159c0b2b93ccd563
lina-jamal <linahjamal89@gmail.com>
2021-03-08 01:31:23 +0200

    make verify function

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,11 +1,14 @@
 const Boom = require("@hapi/boom");
-const jwtFunction = require("./jwt");
+// const jwtFunction = require("./jwt");
+const admin = require("firebase-admin");
 
 const Auth = async (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) return next(Boom.unauthorized("You are not registered yet"));
+  const { tokenID } = req.body;
+  const userId = await admin.auth().verifyIdToken(tokenID);
+  // const { token } = req.cookies;
+  if (!tokenID) return next(Boom.unauthorized("You are not registered yet"));
   try {
-    req.user = await jwtFunction.verifyToken(token);
+    req.user = userId.split("-")[1];
     next();
   } catch (err) {
     return next(Boom.unauthorized("You are not Authorized"));
